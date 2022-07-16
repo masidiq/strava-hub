07bd294241ad5b7cc6dd4bcc95438f26db483d48
Sidiq <makbarsidiq@gmail.com>
2022-07-16 13:21:47 +0700

    text import

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,7 @@
 import Layout from "../components/layout";
 import { Divider } from "@chakra-ui/react";
+
+import { Text } from "@chakra-ui/react/dist/declarations/src";
 export default function Index() {
   return (
     <section>

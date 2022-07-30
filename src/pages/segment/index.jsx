782b48efd64e98d57a2082397c6c975219158a88
Sidiq <makbarsidiq@gmail.com>
2022-07-31 01:14:51 +0700

    tombol request segment

--- src/pages/segment/index.jsx
+++ src/pages/segment/index.jsx
@@ -10,6 +10,7 @@ import {
   Tag,
   Text,
   useDisclosure,
+  VStack,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import segmentService from "@/services/segmentService";
@@ -47,32 +48,50 @@ export default function SegmentList() {
   }
 
   return (
-    <>
+    <Box px="20px">
       <PageHeader title={title} />
 
-      <Stack spacing={0}>
-        {segmentList.map((item, i) => (
-          <SegmentRow
-            p="15px 20px"
-            borderBottomWidth={1}
-            key={item.Id}
-            item={item}
-            as={Link}
-            to={"/segment/" + item.Id}
-          />
-        ))}
+      <Card p="0" mt="10px">
+        <VStack spacing={0} divider={<Divider />} align="stretch">
+          {segmentList.map((item, i) => (
+            <SegmentRow
+              p="10px 15px"
+              key={item.Id}
+              item={item}
+              as={Link}
+              to={"/segment/" + item.Id}
+            />
+          ))}
 
-        {segmentList.length == 0 && (
-          <>
-            <SegmentRowSkeleton />
-            <SegmentRowSkeleton />
-            <SegmentRowSkeleton />
-            <SegmentRowSkeleton />
-            <SegmentRowSkeleton />
-          </>
-        )}
-      </Stack>
+          {segmentList.length == 0 && (
+            <>
+              <SegmentRowSkeleton />
+              <SegmentRowSkeleton />
+              <SegmentRowSkeleton />
+            </>
+          )}
+        </VStack>
+      </Card>
+      <Button
+        w="full"
+        colorScheme="blue"
+        mt="20px"
+        as="a"
+        href="https://docs.google.com/forms/d/e/1FAIpQLSdDBrHNZogYoT7DbASBneAgMA7py1ilapSrPOYQzN3DW5Q1Xg/viewform?usp=sf_link"
+      >
+        Request / Update Segment
+      </Button>
+      <Button
+        w="full"
+        variant="outline"
+        colorScheme="blue"
+        mt="20px"
+        as="a"
+        href="https://docs.google.com/forms/d/e/1FAIpQLSdrkwzCu_yhkLFzoSj6-GAO6fujNH7wc-SH7e60Jh-A9eLbcA/viewform?usp=sf_link"
+      >
+        Kritik & Saran
+      </Button>
       <Box></Box>
-    </>
+    </Box>
   );
 }

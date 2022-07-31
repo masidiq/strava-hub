5f5f5de028878ecac614c57583a519ce5fef9d35
Sidiq <makbarsidiq@gmail.com>
2022-08-01 06:22:45 +0700

    add ga

---
+++ src/router.jsx
@@ -0,0 +1,49 @@
+import ReactGA from "react-ga";
+const TRACKING_ID = "UA-220438183-5"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+
+import Layout from "./components/Layout";
+import AdminLayout from "./components/AdminLayout";
+import Home from "./pages/home";
+import SalasaKahiji from "./pages/salasa-kahiji";
+import GblaLoop from "./pages/gbla-loop";
+import KbpLoop from "./pages/kbp-loop";
+import SegmentList from "./pages/segment";
+import SegmentDetail from "./pages/segment/_id";
+import AdminSegment from "./pages/admin/segment";
+import Admin from "./pages/admin";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { useEffect } from "react";
+
+export default function Router() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+
+          <Route path="salasa-kahiji" element={<SalasaKahiji />} />
+          <Route path="gbla-loop" element={<GblaLoop />} />
+          <Route path="kbp-loop" element={<KbpLoop />} />
+
+          <Route path="segment">
+            <Route path="" element={<SegmentList />} />
+            <Route path=":id" element={<SegmentDetail />} />
+            <Route path=":id/:date" element={<SegmentDetail />} />
+          </Route>
+        </Route>
+
+        <Route path="admin" element={<AdminLayout />}>
+          <Route path="" element={<Admin />} />
+          <Route path="segment/:id" element={<AdminSegment />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}

238f37c1f6b5486901cd9fb15b356c207ca1acaf
Sidiq <makbarsidiq@gmail.com>
2023-06-13 08:25:10 +0700

    no cache

--- src/services/segmentService.js
+++ src/services/segmentService.js
@@ -45,9 +45,9 @@ export default {
     let param = {
       segmentId: id,
     };
-    if (!notUseCache) {
-      notUseCache = false;
-    }
+    // if (!notUseCache) {
+    //   notUseCache = false;
+    // }
 
     return await eqHttp.getJson(endpoint.getListDate, param).catch((error) => {
       if (error.response.status == 403) {
